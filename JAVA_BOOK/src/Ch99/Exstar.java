b57fc87c0d73032288481668f269ccd4bbe04f6e
kshkshksh <ksh931101@naver.com>
2023-06-12 17:19:20 +0900

    V.01새로운별탑쌓기와과자상점

--- JAVA_BOOK/src/Ch99/Exstar.java
+++ JAVA_BOOK/src/Ch99/Exstar.java
@@ -1,45 +1,20 @@
 package Ch99;
 
+import java.util.Scanner;
+
 public class Exstar {
-
-	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-
-	        for (int j = 4; j > i; j--) { // i = 0 j = 4,3,2,1
-
-	                                      // i = 1 j = 4,3,2
-
-	                                      // i = 2 j = 4,3
-
-	                                      // i = 3 j = 4
-
-	                                      // i = 4 --> j는 4부터 시작하여 i보다 클때까지 - 시키는데 4는 j와 같으므로 제외
-
-	            System.out.print(" ");
-
-	        }
-
-	        for (int k = 1; k <= i*2+1; k++) {//k는 1부터 시작
-
-	            System.out.print("*"); //  i 가  0 -->0*2+1 1 1  =         1 
-
-	                                    // i 가 1 -->1*2+1  3 3 <=       1,2,3
-
-	                                    // i 가 2 -->2*2+1  5 5 <=     1,2,3,4,5
-
-	                                    // i 가 3 -->3*2+1  7 7 <=   1,2,3,4,5,6,7
-
-	                                    // i 가 4 -->4*2+1  9 9 <= 1,2,3,4,5,6,7,8,9
-
-	                                    
-
-	                                }
-
-	        System.out.println();
-
-	    }
-
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력해서 같이 탑을 쌓아요! : ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
